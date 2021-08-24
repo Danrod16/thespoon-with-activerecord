77dd0e0906437619dca1c3351f19fa1d41fa9717
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-08-24 09:16:37 +0200

    Feat: added migration address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_070731) do
+ActiveRecord::Schema.define(version: 2021_08_24_071535) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "address"
   end
 
 end
